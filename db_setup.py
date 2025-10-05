96a069a2ed20014d185e0243093bd8872c23e9d0
54856132 <asakhakhimasindi@gmail.com>
2025-10-05 23:40:38 +0200

    Update db_setup.py

--- db_setup.py
+++ db_setup.py
@@ -38,41 +38,41 @@ insert_song(
     "Kelvin Momo & Da Muziqal Chef ft. Tracy, Thatohatsi"
 )
 insert_song(
-    "They say you know when you know"
-    "So, let's face it, you had me at Hello"
-    "Hesitation never helps"
-    "How could this be anything, anything else?"
-    "When all I dream of is your eyes"
-    "All I long for is your touch"
-    "And, darling, something tells me that's enough, mm-mm-mm-mm"
-    "You can say that I'm a fool"
-    "And I don't know very much"
-    "But I think they call this love"
-    "One smile, one kiss, two lonely hearts is all that it takes"
-    "Now, baby, you're on my mind every night, every day"
-    "Good vibrations getting loud"
-    "How could this be anything, anything else?"
-    "When all I dream of is your eyes"
-    "All I long for is your touch"
-    "And, darling, something tells me that's enough, mm-mm-mm-mm"
-    "You can say that I'm a fool"
-    "And I don't know very much"
-    "But I think they call this love"
-    "Oh, I think they call this love"
-    "Hmm, ooh-ooh, mm"
-    "Mm"
-    "What could this be"
-    "Between you and me? Oh, oh"
-    "All I dream of is your eyes"
-    "All I long for is your touch"
-    "And, darling, something tells me, tells me it's enough"
-    "You can say that I'm a fool"
-    "And I don't know very much"
-    "But I think they call—"
-    "Oh, I think they call—"
-    "Yes, I think they call"
-    "This love"
-    "This love",
+    """They say you know when you know
+    So, let's face it, you had me at Hello
+    Hesitation never helps
+    How could this be anything, anything else?
+    When all I dream of is your eyes
+    All I long for is your touch
+    And, darling, something tells me that's enough, mm-mm-mm-mm
+    You can say that I'm a fool
+    And I don't know very much
+    But I think they call this love
+    One smile, one kiss, two lonely hearts is all that it takes
+    Now, baby, you're on my mind every night, every day
+    Good vibrations getting loud
+    How could this be anything, anything else?
+    When all I dream of is your eyes
+    All I long for is your touch
+    And, darling, something tells me that's enough, mm-mm-mm-mm
+    You can say that I'm a fool
+    And I don't know very much
+    But I think they call this love
+    Oh, I think they call this love
+    Hmm, ooh-ooh, mm
+    Mm
+    What could this be
+    Between you and me? Oh, oh
+    All I dream of is your eyes
+    All I long for is your touch
+    And, darling, something tells me, tells me it's enough
+    You can say that I'm a fool
+    And I don't know very much
+    But I think they call—
+    Oh, I think they call—
+    Yes, I think they call
+    This love
+    This love""",
     "I think they call this love",
     "Elliot James Raey"
 )
